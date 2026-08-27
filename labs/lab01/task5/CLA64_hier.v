b04ba7d7bd446e1e17c4c16fcd698d0a2337de82
Adi-25BITS <f20250768@pilani.bits-pilani.ac.in>
2026-08-27 20:03:25 +0000

    lab01 task5

--- labs/lab01/task5/CLA64_hier.v
+++ labs/lab01/task5/CLA64_hier.v
@@ -35,5 +35,59 @@ module cla64_hier(
 );
 
   // TODO: your hierarchical design goes here.
+wire [63:0] p, g;
+  wire [15:0] P_blk, G_blk;
+  wire [16:0] c_blk;
+
+
+  assign #2 p = a ^ b;
+  assign #2 g = a & b;
+
+  
+  genvar k;
+  generate
+    for (k = 0; k < 16; k = k + 1) begin : gen_blk_pg
+      assign #2 P_blk[k] = &p[4*k + 3 : 4*k];
+      assign #4 G_blk[k] = g[4*k + 3] | 
+                           (p[4*k + 3] & g[4*k + 2]) | 
+                           (p[4*k + 3] & p[4*k + 2] & g[4*k + 1]) | 
+                           (p[4*k + 3] & p[4*k + 2] & p[4*k + 1] & g[4*k]);
+    end
+  endgenerate
+
+  
+  assign c_blk[0] = cin;
+  
+  genvar m, n;
+  generate
+    for (m = 0; m < 16; m = m + 1) begin : gen_hier_carries
+      wire [m:0] terms;
+      for (n = 0; n < m; n = n + 1) begin : gen_hier_p_terms
+        assign terms[n] = G_blk[n] & (&P_blk[m : n+1]);
+      end
+      assign terms[m] = cin & (&P_blk[m : 0]);
+      assign #4 c_blk[m+1] = G_blk[m] | (|terms);
+    end
+  endgenerate
+
+ 
+  genvar i;
+  generate
+    for (i = 0; i < 16; i = i + 1) begin : gen_internal_sum
+      wire [2:0] c_int;
+
+      assign #4 c_int[0] = g[4*i] | (p[4*i] & c_blk[i]);
+      assign #4 c_int[1] = g[4*i + 1] | (p[4*i + 1] & g[4*i]) | (p[4*i + 1] & p[4*i] & c_blk[i]);
+      assign #4 c_int[2] = g[4*i + 2] | (p[4*i + 2] & g[4*i + 1]) | (p[4*i + 2] & p[4*i + 1] & g[4*i]) | (p[4*i + 2] & p[4*i + 1] & p[4*i] & c_blk[i]);
+
+   
+      assign #2 sum[4*i]     = p[4*i]     ^ c_blk[i];
+      assign #2 sum[4*i + 1] = p[4*i + 1] ^ c_int[0];
+      assign #2 sum[4*i + 2] = p[4*i + 2] ^ c_int[1];
+      assign #2 sum[4*i + 3] = p[4*i + 3] ^ c_int[2];
+    end
+  endgenerate
+
+  assign cout = c_blk[16];
 
 endmodule
